c11338961614a3086285b5b230220921dc61dd41
Efrain Figueroa <efrain.figue.95@gmail.com>
2021-05-02 13:57:57 -0500

    se agrega iconografía y fragmento header responsive

---
+++ src/main/java/pe/edu/utp/electroplus/controller/Home.java
@@ -0,0 +1,19 @@
+package pe.edu.utp.electroplus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+public class Home {
+
+    /**
+     *
+    @GetMapping("/")
+    String index(Principal principal) {
+        return "index";
+    }
+    */
+}
