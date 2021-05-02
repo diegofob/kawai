c929ac9e9affb219b88879a4fe79caed740582bd
Efrain Figueroa <efrain.figue.95@gmail.com>
2021-05-02 18:41:17 -0500

    merge cambios Efrain a master

--- src/main/java/pe/edu/utp/electroplus/model/Contact.java
+++ src/main/java/pe/edu/utp/electroplus/model/Contact.java
@@ -4,22 +4,22 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
-//import javax.persistence.*;
-//import java.util.Date;
-//
-//@Entity
-//@Data
-//@Table(name = "t_contact")
-//public class Contact {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//    private String name;
-//    private String email;
-//    private String phone;
-//    @DateTimeFormat(pattern = "dd/MM/yyyy")
-//    private Date birtdate;
-//    private String gender;
-//    private String maritalStatus;
-//    private Integer childrens;
-//}
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "t_contact")
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date birtdate;
+    private String gender;
+    private String maritalStatus;
+    private Integer childrens;
+}
